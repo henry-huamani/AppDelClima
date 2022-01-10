f4ffdf0972d50f17d966723125d628e5c8d9f66f
HENRY HUAMANI <huamanihenry22@gmail.com>
2022-01-09 21:38:12 -0500

    Componente Card creado

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,5 +2,24 @@ import React from 'react';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+    <div>
+      <button onClick={props.onClose}>X</button>
+      <div>
+        <h1>{props.name}</h1>
+        <div>
+          <div>
+            <h2>Min</h2>
+            <h2>{props.min}</h2>
+          </div>
+          <div>
+            <h2>Max</h2>
+            <h2>{props.max}</h2>
+          </div>
+          {/* Cuando en un atributo se colocan llaves ya no es necesario colocar las comillas */}
+          <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt="Imagen de clima"/>
+        </div>
+      </div>
+    </div>
+    )
 };
