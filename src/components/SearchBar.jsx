d81c9b9e7d885118326f5bd6f8e9576cc9a7039b
HENRY HUAMANI <huamanihenry22@gmail.com>
2022-01-09 22:06:48 -0500

    Componente SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
-export default function SearchBar(props) {
+export default function SearchBar({onSearch}) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+    // Esta no es buena practica de desarrollo con react
+  function onSearchWithValue(){
+    if(typeof onSearch === "function"){
+      const input = document.getElementById("inputSearch");
+      onSearch(input.value);
+    }
+  }
+  return (
+  <div>
+    <input type="text" placeholder='Ciudad...' id='inputSearch'/>
+    <button onClick={onSearchWithValue}>Agregar</button>
+  </div>
+  )
 };
